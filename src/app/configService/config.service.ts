b3049c0fc1a5d8aae248b4cceca0f31699ee7093
Christian Farias <christian.farias@Christians-MacBook-Pro.local>
2020-03-05 13:11:24 +0100

    change src to json on servie TEMP

--- src/app/configService/config.service.ts
+++ src/app/configService/config.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IConfig } from '../config/config';
+import { IConfig } from '../3D-object-renderer/config';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,41 @@ export class ConfigService {
 
   constructor(private http: HttpClient) { }
 
-  getConfig(){
-    return this.http.get<IConfig>(this._endpoint);
+  getConfig() {
+    // return this.http.get<IConfig>(this._endpoint);
+    return { 
+      "configurationInputs": [ 
+        { 
+          "name": "Color",
+          "type": "color", 
+          "values": [ 
+            { 
+              "label": "Red", "code": "FF0000"
+            }, {
+              "label": "Green", "code": "00FF00" },
+            { 
+              "label": "Blue", "code": "0000FF" },
+            { 
+              "label": "Black", "code": "000000" },
+            { 
+              "label": "White", "code": "FFFFFF"
+            } 
+          ]
+        }
+      ],
+      "parts": [ 
+        {
+          "geometry": "sphere",
+          "parameters": { "radius": "50" }
+        }, {
+          "geometry": "cube",
+          "parameters": { "height": "100" } 
+        }, { 
+          "geometry": "cylinder", "parameters": { 
+          "radius": "50", "height": "100" } 
+        } 
+      ] 
+    }
   }
 
 }
